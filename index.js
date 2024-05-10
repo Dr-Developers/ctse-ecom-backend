9fe2926abfd857a8e5ad7609c95f51c2d4cc0548
Poorna Sankalana <poornasankalana@yahoo.com>
2024-05-11 01:41:09 +0530

    initializing port

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ connectDB();
 
 app.use("/api", router);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
 });
